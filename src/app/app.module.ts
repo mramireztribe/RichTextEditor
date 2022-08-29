8a3670af74bc898e9ceb396f60fc4d16546624b2
Manny Ramirez <mramireztribe@gmail.com>
2022-08-29 13:26:35 -0700

    added textArea

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RichTextEditorComponent } from './components/rich-text-editor/rich-text-editor.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RichTextEditorComponent
   ],
   imports: [
     BrowserModule,
